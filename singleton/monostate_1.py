1d3eb622d9288d91ea45f1e82b353727d03857d9
gabrieldiaslm <xgabrieldias777@gmail.com>
2026-02-13 11:57:48 -0300

    Monostate (Borg) 75% concluído

---
+++ singleton/monostate_1.py
@@ -0,0 +1,29 @@
+# Monostate (Borg)
+
+class StringReprMixin:
+    def __str__(self):
+        params = ', '.join(
+            [f'{k}={v}' for k, v in self.__dict__.items()]
+        )
+        return f'{self.__class__.__name__}({params})'
+    
+    def __rpr__(self):
+        return self.__str__()
+    
+class MonoStateSimple(StringReprMixin):
+    _state = {'x': 10, 'y': 20}
+
+    def __init__(self, nome=None, sobrenome=None):
+        self.__dict__ = self._state
+
+        if nome is not None:
+            self.nome = nome    
+        if sobrenome is not None:
+            self.sobrenome = sobrenome
+
+
+if __name__ == "__main__":
+    m1 = MonoStateSimple(nome='G.')
+    m2 = MonoStateSimple(sobrenome='dias')
+    print(m1)
+    print(m2)
