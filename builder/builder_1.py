edf7423b56243ff6a35b2134705d843e0baf1609
gabrieldiaslm <xgabrieldias777@gmail.com>
2026-02-19 09:46:47 -0300

    builder #1

---
+++ builder/builder_1.py
@@ -0,0 +1,74 @@
+from abc import ABC, abstractmethod
+
+
+class User:
+    def __init__(self):
+        self.firstname = None
+        self.lastname = None
+        self.age = None
+        self.phone_numbers = []
+        self.address = []
+
+class IUserBuilder(ABC):
+    @property
+    @abstractmethod
+    def result(self): pass
+
+    @abstractmethod
+    def add_firstname(self, firstname): pass
+
+    @abstractmethod
+    def add_lastname(self, lastname): pass
+
+    @abstractmethod
+    def add_age(self, age): pass
+    
+    @abstractmethod
+    def add_phone(self, phone): pass
+
+    @abstractmethod
+    def add_address(self, address): pass
+
+class UserBuilder(IUserBuilder):
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self._result = User()
+
+    @property
+    def result(self):
+        return_data = self._result
+        self.reset()
+        return return_data 
+
+    def add_firstname(self, firstname):
+        self._result.firstname = firstname
+
+    def add_lastname(self, lastname): 
+        self._result.lastname = lastname
+
+    def add_age(self, age): 
+        self.result.age = age
+    
+    def add_phone(self, phone): 
+        self._result.phone_numbers.append(phone)
+
+    def add_address(self, address): 
+        self._result.address.append(address)
+
+class UserDirector:
+    def __init__(self, builder: UserBuilder):
+        self._builder = builder
+
+    def with_age(self, firstname, lastname, age):
+        self._builder.add_firstname(firstname)
+        self._builder.add_lastname(lastname)
+        self._builder.add_age(age)
+        return self._builder.result
+    
+if __name__ == "__main__":
+    user_builder = UserBuilder()
+    user_director = UserDirector(user_builder)
+    user1 = user_director.with_age('Ga', 'Dias', 22)
+    print(user1)
---
+++ singleton/monostate_2.py
@@ -0,0 +1,34 @@
+# Monostate (Borg)
+
+class StringReprMixin:
+    def __str__(self):
+        params = ', '.join(
+            [f'{k}={v}' for k, v in self.__dict__.items()]
+        )
+        return f'{self.__class__.__name__}({params})'
+    
+    def __rpr__(self):
+        return self.__str__()
+    
+class MonoStateSimple(StringReprMixin):
+    _state = {}
+
+    def _new(cls, *arg, **kwargs):
+        obj = super().__new__(cls)
+        obj.__dict__ = cls._state
+        return obj
+    
+    def __init__(self, nome=None, sobrenome=None):
+        self.__dict__ = self._state
+
+        if nome is not None:
+            self.nome = nome    
+        if sobrenome is not None:
+            self.sobrenome = sobrenome
+
+
+if __name__ == "__main__":
+    m1 = MonoStateSimple(nome='G.')
+    m2 = MonoStateSimple(sobrenome='dias')
+    print(m1)
+    print(m2)
