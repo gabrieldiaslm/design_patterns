49ef584049a7994db5b2372c4edf6cecd28b0c23
gabrieldiaslm <xgabrieldias777@gmail.com>
2026-02-20 11:37:17 -0300

    state introdução

---
+++ behavioral/state/state_1.py
@@ -0,0 +1,7 @@
+'''
+o padrão de projeto state é um padrão comportamental
+que tem a intenção de permitir a um objeto mudar seu 
+comportamento quando o seu estado interno muda.
+o objeto parecerá ter mudado sua classe.
+'''
+
