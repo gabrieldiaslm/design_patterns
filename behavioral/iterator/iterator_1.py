cfbc1886036456885f8122c7df7dec98b62c6c32
gabrieldiaslm <xgabrieldias777@gmail.com>
2026-02-25 10:46:17 -0300

    iterator

--- behavioral/iterator/iterator_1.py
+++ behavioral/iterator/iterator_1.py
@@ -18,9 +18,75 @@ para um objeto iterador.
 #######################################################
 
 from collections.abc import Iterator, Iterable
+from typing import List, Any
+
+class ReverseIterator(Iterator):
+    def __init__(self, collection: List[Any]) ->None:
+        self._collection = collection
+        self._index = -1
+
+    def next(self):
+        try:
+            return self.__next__()
+        except StopIteration:
+            return None
+
+    def __next__(self):
+        try:
+            item = self._collection[self._index]
+            self._index -= 1
+            return item
+        except IndexError:
+            raise StopIteration
+
 
 class MyIterator(Iterator):
-    ...
+    def __init__(self, collection: List[Any]) ->None:
+        self._collection = collection
+        self._index = 0
+
+    def next(self):
+        try:
+            return self.__next__()
+        except StopIteration:
+            return None
+
+    def __next__(self):
+        try:
+            item = self._collection[self._index]
+            self._index += 1
+            return item
+        except IndexError:
+            raise StopIteration
+
+class MyList(Iterable):
+    def __init__(self) ->None:
+        self._items: List[Any] = []
+        self._my_iterator = MyIterator(self._items)
+
+    def add(self,value: Any)->None:
+        self._items.append(value)
+
+    def __iter__(self):
+        return self._my_iterator
+    
+    def reverse_iterator(self) ->Iterator:
+        return ReverseIterator(self._items)
+
+    def __str__(self)->str:
+        return f'{self.__class__.__name__}({self._items})'
 
 if __name__ == '__main__':
-    iterator = MyIterator() #error
+    mylist = MyList()
+    mylist.add('João')
+    mylist.add('Mago')
+    mylist.add('LLL')
+
+    print(mylist)
+    print('ROUBEI UM VALOR: ', mylist.__iter__().next())
+
+    for value in mylist:
+        print(value)
+    print()
+    for value in mylist.reverse_iterator():
+        print(value)
