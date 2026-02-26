9618f0d81f3e3515b609297538e0fcadc44de1a0
gabrieldiaslm <xgabrieldias777@gmail.com>
2026-02-26 11:29:08 -0300

    proxy inicio

---
+++ structural/proxy/proxy_1.py
@@ -0,0 +1,34 @@
+"""
+O Proxy é um padrão de projeto estrutural que tem a
+intenção de fornecer um objeto substituto que atua
+como se fosse o objeto real que o código cliente
+gostaria de usar.
+O proxy receberá as solicitações e terá controle
+sobre como e quando repassar tais solicitações ao
+objeto real.
+
+Com base no modo como o proxies são usados,
+nós os classificamos como:
+
+- Proxy Virtual: controla acesso a recursos que podem
+ser caros para criação ou utilização.
+- Proxy Remoto: controla acesso a recursos que estão
+em servidores remotos.
+- Proxy de proteção: controla acesso a recursos que
+possam necessitar autenticação ou permissão.
+- Proxy inteligente: além de controlar acesso ao
+objeto real, também executa tarefas adicionais para
+saber quando e como executar determinadas ações.
+
+Proxies podem fazer várias coisas diferentes:
+criar logs, autenticar usuários, distribuir serviços,
+criar cache, criar e destruir objetos, adiar execuções
+e muito mais...
+"""
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from time import sleep
+
+
+class IUser(ABC):
+    ...
