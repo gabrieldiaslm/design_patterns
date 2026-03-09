e35dea9d5b4f3c8c1ed59619e0c70dd9fddbb46a
gabrieldiaslm <xgabrieldias777@gmail.com>
2026-03-09 09:59:50 -0300

    proxy

--- structural/proxy/proxy_1.py
+++ structural/proxy/proxy_1.py
@@ -28,7 +28,78 @@ e muito mais...
 from __future__ import annotations
 from abc import ABC, abstractmethod
 from time import sleep
-
+from typing import List, Dict
 
 class IUser(ABC):
-    ...
+    '''Subject Interface'''
+    firstname: str
+    lastname: str
+    @abstractmethod
+    def get_addresses(self) -> List[Dict]: pass
+
+    @abstractmethod
+    def get_all_user_data(self) -> Dict: pass
+
+class RealUser(IUser):
+    """Real Subject"""
+    def __init__(self, firstname: str, lastname: str) -> None:
+        sleep(2) #simulando requisição (tempo)
+        self.firstname = firstname
+        self.lastname = lastname
+
+
+    def get_addresses(self) -> List[Dict]: 
+        sleep(2) #simluando request
+        return [
+            {'rua': 'dos Bobos', 'numero': 0}
+        ]
+
+    def get_all_user_data(self) -> Dict:
+        sleep(2)
+        return{
+            'cpf': '676.767.676-67',
+            'rg': 'AB111222344'
+        }
+    
+class UserProxy(IUser):
+    def __init__(self, firstname: str, lastname:str) ->None:
+        self.firstname = firstname
+        self.lastname = lastname
+        self._real_user: RealUser #lazy instance
+
+    #Esses objetos nao existem ainda nesse ponto do codigo
+        self._cached_addresses: List[Dict]
+        self._all_user_data: Dict
+
+    def get_real_user(self) ->None:
+        if not hasattr(self, '_real_user'):
+            self._real_user= RealUser(self.firstname, self.lastname)
+    
+    def get_addresses(self) -> List[Dict]:
+        self.get_real_user()
+
+        if not hasattr(self, '_cached_addresses'):
+            self._cached_addresses = self._real_user.get_addresses()
+        
+        return self._cached_addresses
+
+    def get_all_user_data(self) -> Dict:
+        self.get_real_user()
+        
+        if not hasattr(self, '_all_user_data'):
+            self._all_user_data = self._real_user.get_all_user_data()
+        
+        return self._all_user_data
+
+if __name__ == '__main__':
+    gdias = UserProxy('Gabriel', 'Dias')
+    print(gdias.firstname)
+    print(gdias.lastname)
+
+    print(gdias.get_all_user_data())
+    print(gdias.get_addresses())
+    print()
+    print('CACHED DATA:')
+    for i in range(50):
+        print(gdias.get_all_user_data())
+        print(gdias.get_addresses())
